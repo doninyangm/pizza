5b4bc0704617d4250f02419439f332be3d9980c2
Andre Yang <doninyangm@gmail.com>
2020-06-30 20:01:22 +0100

    user migigration fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Schema;
 use App\User;
 
 class CreateUsersTable extends Migration
@@ -39,5 +40,9 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+    }
+
+    public function boot(){
+        Schema::defaultStringLength(191);
     }
 }
